9d49787d63794c6d02f2bd543cb0cc1f47ff56b4
SB- Alumno <SB-Alumno@duoc.acad>
2026-03-31 15:02:24 -0300

    Solucionado conflicto de repositorio embebido

---
+++ FS3_CRR/order-service/order-service/src/main/java/com/empresa/order_service/OrderServiceApplication.java
@@ -0,0 +1,24 @@
+package com.empresa.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
+
+@Configuration
+class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
+	}
+}
---
+++ FS3_CRR/payment-service/payment-service/src/main/java/com/empresa/payment_service/PaymentServiceApplication.java
@@ -0,0 +1,24 @@
+package com.empresa.payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
+
+@Configuration
+class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+    	return new RestTemplate();
+	}
+}
